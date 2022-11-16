3093cd34890200a8f922535bb40047f9b12596f7
Duncan <82653946+duncqn@users.noreply.github.com>
2022-11-17 00:01:51 +0100

    Ready for accessment

--- Controller/Data.cs
+++ Controller/Data.cs
@@ -21,10 +21,10 @@ namespace Controller
 
         public static void AddParticipants()
         {
-            Competition.Participants.Add(new Driver("Duncan", new Car(19, 10, 20, false), IParticipant.TeamColors.Red));
-            Competition.Participants.Add(new Driver("Noa", new Car(18, 10, 19, false), IParticipant.TeamColors.Green));
-            Competition.Participants.Add(new Driver("Michiel",new Car(16, 10, 17, false), IParticipant.TeamColors.Blue));
-            Competition.Participants.Add(new Driver("Damian",new Car(20, 10, 14, false), IParticipant.TeamColors.Yellow));
+            Competition.Participants.Add(new Driver("Driver1", new Car(19, 10, 20, false), IParticipant.TeamColors.Red));
+            Competition.Participants.Add(new Driver("Driver2", new Car(18, 10, 19, false), IParticipant.TeamColors.Green));
+            Competition.Participants.Add(new Driver("Driver3",new Car(16, 10, 17, false), IParticipant.TeamColors.Blue));
+            Competition.Participants.Add(new Driver("Driver4",new Car(20, 10, 14, false), IParticipant.TeamColors.Yellow));
         }
 
         public static void AddTracks()
---
+++ ControllerTest/Controller_Race_GetSectionData.cs
@@ -0,0 +1,44 @@
+﻿using Controller;
+using Model;
+using static Model.IParticipant;
+
+namespace ControllerTest
+{
+    [TestFixture]
+    public class Controller_Race_GetSectionData
+    {
+        private Race _testRace { get; set; }
+        private Track _track { get; set; }
+        private List<IParticipant> _participants { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Data.Initialize();
+            Data.NextRace();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Data.CurrentRace.CleanUp();
+            Data.CurrentRace = null;
+        }
+        
+
+        [Test]
+        public void GetSectionData_ReturnsData()
+        {
+            _track = new Track("test", new SectionTypes[] { SectionTypes.StartGrid, SectionTypes.Straight, SectionTypes.LeftCorner, SectionTypes.RightCorner, SectionTypes.Finish });
+            _participants = new List<IParticipant>();
+            _participants.Add(new Driver("Driver1", new Car(1, 1, 1, false),TeamColors.Blue));
+            _testRace = new Race(_track, _participants);
+
+            foreach (var section in Data.CurrentRace.Track.Sections)
+            {
+                var SectionData = Data.CurrentRace.GetSectionData(section);
+                Assert.NotNull(SectionData);
+            }
+        }
+    }
+}
